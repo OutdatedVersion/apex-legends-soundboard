ebbe628e413e24fe86917a3a7c3d5a3ff927253c
Ben Watkins <ben@outdatedversion.com>
2020-06-19 13:37:13 -0500

    Initial Commit

---
+++ Apex Legends Sounds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apex Legends Sounds
+//
+//  Created by Ben Watkins on 6/19/20.
+//  Copyright © 2020 outdatedversion LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Apex Legends SoundsTests/Apex_Legends_SoundsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Apex_Legends_SoundsTests.swift
+//  Apex Legends SoundsTests
+//
+//  Created by Ben Watkins on 6/19/20.
+//  Copyright © 2020 outdatedversion LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Apex_Legends_Sounds
+
+class Apex_Legends_SoundsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
